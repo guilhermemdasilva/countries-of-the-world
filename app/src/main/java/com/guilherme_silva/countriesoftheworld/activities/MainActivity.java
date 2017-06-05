2529ec5094d2d8b4466e3168c6051f22e3a258f6
Guilherme Silva <guilherme.mdasilva@gmail.com>
2017-06-05 02:19:51 -0400

    Added Client and its AsyncTask to perform network tasks

--- app/src/main/java/com/guilherme_silva/countriesoftheworld/activities/MainActivity.java
+++ app/src/main/java/com/guilherme_silva/countriesoftheworld/activities/MainActivity.java
@@ -1,13 +1,11 @@
 package com.guilherme_silva.countriesoftheworld.activities;
 
 import android.Manifest;
-import android.app.FragmentManager;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -16,46 +14,29 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.support.v7.widget.SearchView;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+
 import com.guilherme_silva.countriesoftheworld.R;
-import com.guilherme_silva.countriesoftheworld.fragments.CountryDialogFragment;
-import com.guilherme_silva.countriesoftheworld.models.Country;
-import com.guilherme_silva.countriesoftheworld.views.adapters.CountryAdapter;
+import com.guilherme_silva.countriesoftheworld.network.ClientAsyncTask;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
-    @BindString(R.string.url_countries_endpoint) String ENDPOINT;
     @BindString(R.string.permission_title) String PERMISSION_TITLE;
     @BindString(R.string.permission_msg) String PERMISSION_MSG;
     @BindString(R.string.button_label_ok) String OK_BUTTON;
-    @BindString(R.string.error_title) String ERROR_TITLE;
-    @BindString(R.string.error_msg) String ERROR_MSG;
 
     @BindView(R.id.rvCountriesList) RecyclerView recyclerView;
 
-    private ArrayList<String> countriesNames = new ArrayList<>();
-    private RequestQueue requestQueue;
-    private Gson gson;
     private LinearLayoutManager linearLayoutManager;
     private final int PERMISSION_REQUEST_INTERNET = 1;
+    private ArrayList<String> countriesNames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,73 +48,10 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(linearLayoutManager);
         handlePermissions();
         if (arePermissionsGranted()) {
-            initializeClient();
-            initializeGson();
-            fetchCountries();
+           new ClientAsyncTask(this, recyclerView, countriesNames).execute();
         }
         handleIntent(getIntent());
     }
-
-    private void initializeClient() {
-        requestQueue = Volley.newRequestQueue(this);
-    }
-
-    private void initializeGson() {
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gson = gsonBuilder.create();
-    }
-
-    public void fetchCountries() {
-        StringRequest request = new StringRequest(Request.Method.GET, ENDPOINT, onCountriesLoaded, onCountriesError);
-        requestQueue.add(request);
-    }
-
-    private final Response.Listener<String> onCountriesLoaded = new Response.Listener<String>() {
-        @Override
-        public void onResponse(String response) {
-            List<Country> countries = Arrays.asList(gson.fromJson(response, Country[].class));
-            for(Country country : countries) {
-                countriesNames.add(country.getName());
-            }
-            CountryAdapter countryAdapter = new CountryAdapter(countries, new CountryAdapter.OnCountryItemClicked() {
-                @Override
-                public void OnCountryInteraction(Country countryInfo) {
-                    FragmentManager fm = getFragmentManager();
-                    Bundle args = new Bundle();
-                    CountryDialogFragment countryDialogFragment = new CountryDialogFragment ();
-                    args.putString("name", countryInfo.getName());
-                    args.putString("alpha2Code", countryInfo.getAlpha2Code());
-                    args.putString("capital", countryInfo.getCapital());
-                    args.putString("subregion", countryInfo.getSubregion());
-                    args.putInt("population", countryInfo.getPopulation());
-                    args.putFloat("area", countryInfo.getArea());
-                    countryDialogFragment.setArguments(args);
-                    countryDialogFragment.show(fm, "Country Dialog Fragment");
-                }
-            });
-            recyclerView.setAdapter(countryAdapter);
-            Point size = new Point();
-            getWindowManager().getDefaultDisplay().getSize(size);
-            CountryAdapter.BottomDecoration bottomOffsetDecoration = new CountryAdapter.BottomDecoration(size.y);
-            recyclerView.addItemDecoration(bottomOffsetDecoration);
-        }
-    };
-
-    private final Response.ErrorListener onCountriesError = new Response.ErrorListener() {
-        @Override
-        public void onErrorResponse(VolleyError error) {
-            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-            alertDialog.setTitle(ERROR_TITLE);
-            alertDialog.setMessage(ERROR_MSG);
-            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, OK_BUTTON,
-                    new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    });
-            alertDialog.show();
-        }
-    };
 
     private void handlePermissions() {
         if(!arePermissionsGranted()) {
@@ -158,9 +76,7 @@ public class MainActivity extends AppCompatActivity {
         switch(requestCode) {
             case PERMISSION_REQUEST_INTERNET:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    initializeClient();
-                    initializeGson();
-                    fetchCountries();
+                    new ClientAsyncTask(this, recyclerView, countriesNames).execute();
                 } else {
                     AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
                     alertDialog.setTitle(PERMISSION_TITLE);
---
+++ app/src/main/java/com/guilherme_silva/countriesoftheworld/network/ClientAsyncTask.java
@@ -0,0 +1,69 @@
+package com.guilherme_silva.countriesoftheworld.network;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.graphics.Point;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.guilherme_silva.countriesoftheworld.fragments.CountryDialogFragment;
+import com.guilherme_silva.countriesoftheworld.models.Country;
+import com.guilherme_silva.countriesoftheworld.views.adapters.CountryAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientAsyncTask extends AsyncTask<String, String, List<Country>> {
+    private Context context;
+    private RecyclerView recyclerView;
+    private ArrayList<String> countriesNames;
+
+    public ClientAsyncTask(Context context, RecyclerView recyclerView, ArrayList<String> countriesNames) {
+        this.context = context;
+        this.recyclerView = recyclerView;
+        this.countriesNames = countriesNames;
+    }
+
+    @Override
+    protected List<Country> doInBackground(String... params) {
+        Client client = new Client(context);
+        return client.getCountries();
+    }
+
+    @Override
+    protected void onPostExecute(List<Country> countries) {
+        for(Country country : countries) {
+            countriesNames.add(country.getName());
+        }
+        recyclerView.setAdapter(new CountryAdapter(countries, new CountryAdapter.OnCountryItemClicked() {
+            @Override
+            public void OnCountryInteraction(Country countryInfo) {
+                makeCountryFragment(countryInfo);
+            }
+        }));
+        addEmptyItemBottom();
+    }
+
+    private void makeCountryFragment(Country countryInfo) {
+        FragmentManager fm = ((Activity) context).getFragmentManager();
+        Bundle args = new Bundle();
+        CountryDialogFragment countryDialogFragment = new CountryDialogFragment ();
+        args.putString("name", countryInfo.getName());
+        args.putString("alpha2Code", countryInfo.getAlpha2Code());
+        args.putString("capital", countryInfo.getCapital());
+        args.putString("subregion", countryInfo.getSubregion());
+        args.putInt("population", countryInfo.getPopulation());
+        args.putFloat("area", countryInfo.getArea());
+        countryDialogFragment.setArguments(args);
+        countryDialogFragment.show(fm, "Country Dialog Fragment");
+    }
+
+    private void addEmptyItemBottom() {
+        Point size = new Point();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getSize(size);
+        CountryAdapter.BottomDecoration bottomOffsetDecoration = new CountryAdapter.BottomDecoration(size.y);
+        recyclerView.addItemDecoration(bottomOffsetDecoration);
+    }
+}
